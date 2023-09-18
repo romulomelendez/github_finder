74cec1d1bba9fe4842d62dad3d5cec73e7f17106
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-09-18 20:33:53 -0300

    App are fetching github profile datas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-const Home = () => <h1>MOJO</h1>
+"use client"
+
+import { useState } from "react"
+
+const Home = () => {
+
+    const [username, setUsername] = useState("")
+
+    const getGitHubProfile = async () => {
+
+        const data = await fetch(`https://api.github.com/users/${username}/repos`)
+        const profileData = await data.json()
+        console.log(profileData)
+    }
+
+    return (
+        <>
+            <input type="text" className="bg-gray-950 text-white" onChange={(event) => setUsername(event.target.value)} value={username} />
+            <button type="button" onClick={getGitHubProfile}>Search</button>
+        </>
+    )
+}
 
 export default Home
